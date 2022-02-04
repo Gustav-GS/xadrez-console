1d018118af51fad32070196718a2a68b74af7f59
Gustav-GS <gusta.sant98@gmail.com>
2022-02-04 16:02:59 -0300

    Algumas alteracões nas classes e iniciando a criação da tela.

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -3,8 +3,9 @@ using tabuleiro;
 namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
-            Tabuleiro tab = new Tabuleiro();
-
+            Tabuleiro tab = new Tabuleiro(8,8);
+            Tela.imprimirTabuleiro(tab);
+            Console.ReadLine();
 
             }
         }
--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -1,6 +1,4 @@
-﻿
-
-namespace tabuleiro {
+﻿namespace tabuleiro {
     class Peca {
         public Posicao posicao { get; set; }
         public Cor cor { get;protected set; }
